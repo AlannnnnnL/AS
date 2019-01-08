b99a1b0960fac9a3e24c972295a7a25d14d6a924
AlannnnnnL <lhf_99280@163.com>
2019-01-08 16:35:27 +0800

    删除默认头像

--- pages/user/user.js
+++ pages/user/user.js
@@ -3,7 +3,7 @@ let app = getApp();
 Page({
   // 页面的初始数据
   data: {
-    avatarUrl: 'https://thumbnail0.baidupcs.com/thumbnail/4506dc5c1417859b009438798e4f4ee3?fid=1061451524-250528-103909565181044&time=1546077600&rt=sh&sign=FDTAER-DCb740ccc5511e5e8fedcff06b081203-Me3BgR%2FQy0ln4e7Ls4fQe83wDDA%3D&expires=8h&chkv=0&chkbd=0&chkpc=&dp-logid=8413385499484523359&dp-callid=0&size=c710_u400&quality=100&vuk=-&ft=video', // 头像
+    avatarUrl: '', // 头像
   },
   // 生命周期函数--监听页面加载
   onLoad: function (options) {
